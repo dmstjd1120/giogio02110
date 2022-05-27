2b87cc067dd84b1f516ecf463334b86d8c2d54f7
eun2005 <giogio02110@gmail.com>
2022-05-27 14:22:08 +0900

    220527

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>링크를 누르세여
         </p>
         <a
           className="App-link"
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
